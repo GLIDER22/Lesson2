0d3d0a7638d4e9c04faddc62b4ee3a88edd77d6c
GLIDER22 <kotec0813@gmail.com>
2024-11-20 14:07:46 +0300

    Урок 2 Арифметика

---
+++ main.py
@@ -0,0 +1,29 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print('1st program')
+print((9**0.5)*5)
+print("2nd program")
+print(9,99>9,98 and 1000!=1000.1)
+print('3rd program')
+print((2*2+2)==((2+2)*2))
+print('4th program')
+str_a= '123.456'
+b=10
+c=10
+d=(float(str_a)*b)%c
+print('The value of d=',d)
+
